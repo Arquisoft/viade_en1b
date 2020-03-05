d083282d4f4297704f84e97269d0bb0d493800f8
els <navesllanes@gmail.com>
2020-03-05 21:55:38 +0100

    Profile started being developed

--- src/components/user/myProfile/MyProfile.js
+++ src/components/user/myProfile/MyProfile.js
@@ -1,9 +1,23 @@
-import React from 'react'
-
+import React from 'react';
+import './MyProfile.css';
 export default function MyProfile() {
     return (
-        <div>
-            
+        <div className="GeneralComponent">
+		<section>
+        		<h1> Profile </h1> 
+			<div className="ProfileData">
+				<p>Name: </p>
+	    			<p>WebID: </p>
+	    			<p>Country: </p>
+	    			<p>Email: </p>
+	    			<p>Nº friends: </p>
+	    		</div>
+    			<h1> Routes data </h1>
+			<div className="ProfileData">			
+				<p>Nº of routes stored: </p>
+				<p>Nº of routes shared: </p>
+			</div>
+	    	</section>
         </div>
     )
 }
