bacab7d98e976ad33b4102105e54b2200e0c2cfa
els <navesllanes@gmail.com>
2020-03-07 22:45:08 +0100

    Profile semi-done, remains routes and friends

--- src/components/user/myProfile/MyProfile.js
+++ src/components/user/myProfile/MyProfile.js
@@ -1,25 +1,65 @@
-import React from 'react';
-import { Image,  useWebId, Value } from '@solid/react';
+import React, {useEffect, useContext,   setState, useState, useRef}  from 'react';
+import { Image, useWebId, Value } from '@solid/react';
+import data from '@solid/query-ldflex' 
 import './MyProfile.css';
+import { Button,  Row, Col, Container } from 'react-bootstrap';
+
+
+
+
+
+
 export default function MyProfile() {
-    return (
-        <div className="GeneralComponent">
-		<section>
-        		<h1> Profile </h1> 
-			<div className="ProfileData">
-				<p>Name: <Value  src="user.name"/> </p>
-	    			<p>WebID: {useWebId()} </p>
-	    			<p>Role: <Value src="user.role"/> </p>
-	    			<p>Country: </p>
-	    			<p>Email: </p>
-	    			<p>Nº friends: </p>
-	    		</div>
-    			<h1> Routes data </h1>
-			<div className="ProfileData">			
-				<p>Nº of routes stored: </p>
-				<p>Nº of routes shared: </p>
-			</div>
-	    	</section>
+
+	const  [theEmail , setTheEmail] = useState(""); 
+
+
+	const getEmail = async () =>  {
+
+		const  emailsId = await data.user[ 'http://www.w3.org/2006/vcard/ns#hasEmail' ].value ;	
+		const firstEmail = await data[emailsId].vcard_value.value; 
+		const emailParsed = firstEmail.split(":")[1]
+		setTheEmail(emailParsed) 
+
+	}
+	getEmail() ; 
+	
+	const redirect = () => {
+
+    		window.location.href = "/dashboard" ;
+
+	}
+
+
+
+	return (
+        <div className="GeneralComponent " >
+		<Container className="Container">
+			<Row>
+				<Col>
+				<div className="AllData">
+					<h1> Profile </h1> 
+				<div className="ProfileData">
+						<p>Name: <Value  src="user.name"/> </p>
+						<p>WebID: {useWebId()} </p>
+						<p>Role: <Value src="user.vcard_role"/> </p>
+						<p>Email: {theEmail} </p>
+						<p>Friends list:  </p>
+					</div>
+					<h1> Routes data </h1>
+					<div className="ProfileData">			
+						<p>Nº of routes stored: </p>
+						<p>Nº of routes shared: </p>
+					</div>
+				</div>
+				</Col>
+				<Col>
+					<Image src="user.vcard_hasPhoto" defaultSrc="profile.svg" className="profile profilePicture"/>
+				</Col>
+			</Row>
+	
+	<button onClick={redirect}> Return  </button>	
+	    	</Container>
         </div>
     )
 }
