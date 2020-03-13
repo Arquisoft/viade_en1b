aae1504aa9023be7d881a36b2a1c21d5b2aae6d0
els <navesllanes@gmail.com>
2020-03-13 19:30:56 +0100

    adding toggle buttons

--- src/components/settings/Settings.js
+++ src/components/settings/Settings.js
@@ -1,13 +1,39 @@
 import React from 'react'
 import './Settings.css'
+import { ToggleButton, ButtonGroup } from 'react-bootstrap';
+
+
 
 export default function Settings(props) {
+	
+
+	const setTheme = (params) => {
+
+	var paramValue = params.target.value;
+
+
+	}
+
+
+
 
 
 	return(
 		<div className="GeneralComponent">
 
 			<h1> Settings </h1>
+			<ButtonGroup toggle onChange={setTheme} > 
+				<ToggleButton type="radio" name="radio" defaultChecked value="Normal">
+					Normal
+			    	</ToggleButton>
+				<ToggleButton type="radio" name="radio" value="Dark">				      
+					Dark 
+				</ToggleButton>
+				<ToggleButton type="radio" name="radio" value="Blind">
+					Blind
+				</ToggleButton>
+
+			</ButtonGroup>
 		</div> 
 	)
 }
