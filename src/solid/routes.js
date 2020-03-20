1b4326a06a65e9aa3e97dd04b9601f9b5280c3ef
InigoGutierrez <inigogf.95@gmail.com>
2020-03-20 20:09:29 +0100

    New pod routes interaction functions.

--- src/solid/routes.js
+++ src/solid/routes.js
@@ -3,22 +3,51 @@ import FC from "solid-file-client";
 import { getWebId } from "./auth";
 
 export async function uploadRouteToPod(route) {
-    var session = await getWebId();
-    var url = session.split("/profile")[0] + "/public/routes/";
-    var fc = new FC(auth);
+    let url = await getRoutesFolder();
+    let fc = new FC(auth);
     if(!await fc.itemExists(url)){
         await fc.createFolder(url);
     }
     await fc.createFile(url + route.name, JSON.stringify(route), "text/plain");
 }
 
+export async function getRouteFromPod(routeName) {
+    let url = await getRoutesFolder();
+    let fc = new FC(auth);
+    let folder = await fc.readFolder(url);
+    if (folder.files.includes(routeName))
+        return fc.readFile(url + routeName);
+    return null;
+}
+
 export async function getRoutesFromPod() {
-    var session = await getWebId();
-    var url = session.split("/profile")[0] + "/public/routes/";
-    var fc = new FC(auth);
-    var folder = await fc.readFolder(url);
-    var routesTexts = await Promise.all(folder.files.map(async (f) => await fc.readFile(url + f.name)));
-    var routes = routesTexts.map(JSON.parse);
+    let url = await getRoutesFolder();
+    let fc = new FC(auth);
+    let folder = await fc.readFolder(url);
+    let routesTexts = await Promise.all(folder.files.map(async (f) => await fc.readFile(url + f.name)));
+    let routes = routesTexts.map(JSON.parse);
     return routes;
+}
+
+export async function clearRoutesFromPod() {
+    let url = await getRoutesFolder();
+    let fc = new FC(auth);
+    let folder = await fc.readFolder(url);
+    await Promise.all(folder.files.map(async (f) => await fc.deleteFile(url + f.name)));
+}
+
+export async function clearRouteFromPod(routeName) {
+    let url = await getRoutesFolder();
+    let fc = new FC(auth);
+    let folder = await fc.readFolder(url);
+    if (folder.files.includes(routeName)) {
+        let fileUrl = url + routeName;
+        fc.deleteFile(fileUrl);
+    }
+}
+
+export async function getRoutesFolder() {
+    let session = await getWebId();
+    return session.split("/profile")[0] + "/public/routes/";
 }
 
