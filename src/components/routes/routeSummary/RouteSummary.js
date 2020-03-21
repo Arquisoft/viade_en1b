4d7eed026eaf72a2f8fbf947f0d01f50cb1d0ba7
César García Cabeza <cesareogarcia@hotmail.es>
2020-03-21 13:01:39 +0100

    PageNotFound test

--- src/components/routes/routeSummary/RouteSummary.js
+++ src/components/routes/routeSummary/RouteSummary.js
@@ -16,3 +16,5 @@ export const RouteSummary = (props) => {
     )
 }
 
+export default RouteSummary;
+
--- src/components/routes/routeSummary/tests/RouteSummary.test.js
+++ src/components/routes/routeSummary/tests/RouteSummary.test.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { render, fireEvent, queryByTestId, waitForElement } from '@testing-library/react'
+import { render, fireEvent, queryByTestId } from '@testing-library/react'
 import '@testing-library/jest-dom'
 import { RouteSummary } from '../RouteSummary'
 
