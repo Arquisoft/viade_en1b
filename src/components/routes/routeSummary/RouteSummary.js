d777782c29677a54e2ec24d304f5af1bb646c701
César García Cabeza <cesareogarcia@hotmail.es>
2020-03-21 12:14:04 +0100

    RouteSummary tested

--- src/components/routes/routeSummary/RouteSummary.js
+++ src/components/routes/routeSummary/RouteSummary.js
@@ -2,18 +2,17 @@ import React from 'react'
 import { Card } from 'react-bootstrap'
 import './RouteSummary.css'
 
-function RouteSummary(props) {
+export const RouteSummary = (props) => {
     const {route} = props
     const {onClickHandle} = props
     return (
-        <Card id={props.id} onClick={() => onClickHandle(route)}>
+        <Card data-testid = 'route-summary-card' id={props.id} onClick={() => onClickHandle(route)}>
             <div id="blurryBackground"></div>
             <Card.Body>
-                <Card.Title>{route.name}</Card.Title>
-                <Card.Subtitle>{route.author}</Card.Subtitle>
+                <Card.Title data-testid="route-summary-title">{route.name}</Card.Title>
+                <Card.Subtitle data-testid="route-summary-subtitle">{route.author}</Card.Subtitle>
             </Card.Body>
         </Card>
     )
 }
 
-export default (RouteSummary)
