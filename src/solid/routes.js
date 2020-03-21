6db3e7ccfab1104712851048e74f19487cf55104
InigoGutierrez <inigogf.95@gmail.com>
2020-03-21 19:42:40 +0100

    Minor code quality improvements.

--- src/solid/routes.js
+++ src/solid/routes.js
@@ -8,23 +8,9 @@ import { getWebId } from "./auth";
  * so they act as a coherent module for other parts of the application to use.
  */
 
-export async function uploadRouteToPod(route) {
-    route.id = await getNextId();
-    let url = await getRoutesFolder();
-    let fc = new FC(auth);
-    if(!await fc.itemExists(url)){
-        await fc.createFolder(url);
-    }
-    await fc.createFile(url + route.name, JSON.stringify(route), "text/plain");
-}
-
-export async function getRouteFromPod(routeName) {
-    let url = await getRoutesFolder();
-    let fc = new FC(auth);
-    let folder = await fc.readFolder(url);
-    if (folder.files.includes(routeName))
-        return fc.readFile(url + routeName);
-    return null;
+export async function getRoutesFolder() {
+    let session = await getWebId();
+    return session.split("/profile")[0] + "/public/routes/";
 }
 
 export async function getRoutesFromPod() {
@@ -34,6 +20,37 @@ export async function getRoutesFromPod() {
     let routesTexts = await Promise.all(folder.files.map(async (f) => await fc.readFile(url + f.name)));
     let routes = routesTexts.map(JSON.parse);
     return routes;
+}
+
+async function getNextId() {
+    let routes = await getRoutesFromPod();
+    routes = routes.sort( (a, b) => { return a.id - b.id; } );
+    for (let i = 0; i < routes.length; i++) {
+        if (i !== routes.get(i).id) {
+            return i;
+        }
+    }
+    return routes.length;
+}
+
+export async function uploadRouteToPod(route) {
+    route.id = await getNextId();
+    let url = await getRoutesFolder();
+    let fc = new FC(auth);
+    if (!await fc.itemExists(url)) {
+        await fc.createFolder(url);
+    }
+    await fc.createFile(url + route.name, JSON.stringify(route), "text/plain");
+}
+
+export async function getRouteFromPod(routeName) {
+    let url = await getRoutesFolder();
+    let fc = new FC(auth);
+    let folder = await fc.readFolder(url);
+    if (folder.files.includes(routeName)) {
+        return fc.readFile(url + routeName);
+    }
+    return null;
 }
 
 export async function clearRoutesFromPod() {
@@ -51,19 +68,5 @@ export async function clearRouteFromPod(routeName) {
         let fileUrl = url + routeName;
         fc.deleteFile(fileUrl);
     }
-}
-
-export async function getRoutesFolder() {
-    let session = await getWebId();
-    return session.split("/profile")[0] + "/public/routes/";
-}
-
-async function getNextId() {
-    let routes = await getRoutesFromPod();
-    routes = routes.sort( (a, b) => { return a.id - b.id } );
-    for (let i = 0; i < routes.length; i++)
-        if (i !== routes[i].id)
-            return i;
-    return routes.length;
 }
 
