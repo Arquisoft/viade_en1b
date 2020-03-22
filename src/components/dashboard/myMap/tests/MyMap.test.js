f0b62ace22b34f4ab23584a65f40f2e1129c2a4c
César García Cabeza <cesareogarcia@hotmail.es>
2020-03-23 00:58:50 +0100

    Updated test

--- src/components/dashboard/myMap/tests/MyMap.test.js
+++ src/components/dashboard/myMap/tests/MyMap.test.js
@@ -1,20 +1,24 @@
 import { render, queryByTestId, waitForElement } from "@testing-library/react";
 import React from 'react'
-import { myMap } from "../MyMap";
+import MyMap, { myMap } from "../MyMap";
 
 describe('MyMap Component', () => {
 
     let wrapper;
+    let wrapperb;
     beforeEach(() => {
         const props = {
             center: [[43.360976539, -5.831938919]],
             positions: []
         };
         wrapper = myMap(props.center, props.positions);
+        wrapperb = <MyMap center={props.center} positions={props.positions}></MyMap>
     });
     
     test('Component renders correctly', () => {
         waitForElement(() => {
+            expect(wrapperb.prop('positions')).not.toBeNull();
+            expect(wrapperb.prop('center')).not.toBeNull();
             expect(queryByTestId(wrapper, 'mymap-container')).not.toBeNull();
             expect(queryByTestId(wrapper, 'mymap-map')).not.toBeNull();
             expect(queryByTestId(wrapper, 'mymap-tilelayer')).not.toBeNull();
