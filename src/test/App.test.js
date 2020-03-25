cce4c7d233bf4ca00456d7a61440ee78c30f8ccd
els <navesllanes@gmail.com>
2020-03-25 10:20:35 +0100

    App.js test finished

--- src/test/App.test.js
+++ src/test/App.test.js
@@ -1,25 +1,19 @@
-import { render, queryByTestId, fireEvent, waitForDomChange } from "@testing-library/react";
+import {
+  render,
+  queryByTestId
+} from "@testing-library/react";
 import App from "../App.js";
 import React from "react";
-import { useLoggedIn,  } from "@solid/react";
 
 let wrapper;
-let myMockTheme;
-let myMockLanguage;
 beforeEach(() => {
-    
-    const {container} = render(<App />);
-    wrapper = container;
-    
+  const { container } = render(<App />);
+  wrapper = container;
 });
 
-describe('App Component', () => {
-
-    test('Component renders correctly', () => {
-        expect(queryByTestId(wrapper, 'theApp')).not.toBeNull();
-        expect(queryByTestId(wrapper, 'theNavBar')).toBeNull();
-    });
-
-   
-
+describe("App Component", () => {
+  test("Component renders correctly", () => {
+    expect(queryByTestId(wrapper, "theApp")).not.toBeNull();
+    expect(queryByTestId(wrapper, "theNavBar")).toBeNull();
+  });
 });
