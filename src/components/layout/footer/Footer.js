4e6728e1bf55c159b1fd677eec2da161d4014b79
César García Cabeza <cesareogarcia@hotmail.es>
2020-03-25 10:29:50 +0100

    Footer test

--- src/components/layout/footer/Footer.js
+++ src/components/layout/footer/Footer.js
@@ -2,13 +2,15 @@ import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGithub, faReact } from '@fortawesome/free-brands-svg-icons';
 import './Footer.css';
-export default function Footer() {
+export function Footer() {
 	
 	return (
-        <div id="footer">
+        <div data-tesid = "footer" id="footer">
             <div data-testid="footer-team" className="footerElement"><b>Dev team: </b> Viade_en1b </div>
-            <div data-testid="footer-github" className="footerElement"><a href="https://github.com/Arquisoft/viade_en1b"><FontAwesomeIcon id="githubIcon" icon={faGithub} /></a></div>
-            <div data-testid="footer-react" className="footerElement"><a href="https://reactjs.org/"><FontAwesomeIcon id="githubIcon" icon={faReact} /></a></div>
+            <div data-testid="footer-github" className="footerElement"><a href="https://github.com/Arquisoft/viade_en1b"><FontAwesomeIcon data-testid = "icon-github" id="githubIcon" icon={faGithub} /></a></div>
+            <div data-testid="footer-react" className="footerElement"><a href="https://reactjs.org/"><FontAwesomeIcon id="githubIcon" data-testid="icon-react" icon={faReact} /></a></div>
 	</div>
-	)
-}
+    );
+};
+
+export default Footer();
