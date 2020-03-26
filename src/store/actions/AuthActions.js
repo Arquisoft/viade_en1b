679eabdf937e4eae3afa544d925969c8ddd78224
César García Cabeza <cesareogarcia@hotmail.es>
2020-03-26 11:37:46 +0100

    Redux auth updated

--- src/store/actions/AuthActions.js
+++ src/store/actions/AuthActions.js
@@ -1,22 +1,3 @@
-export const logIn = userWebId => {
-  return (dispatch, getState) => {
-    dispatch({
-      type: "LOG_IN",
-      payload: userWebId
-    });
-  };
-};
-
-export const logOut = () => {
-  return (dispatch, getState) => {
-    //make async code
-    getState().auth.userWebId = null;
-    dispatch({
-      type: "LOG_OUT"
-    });
-  };
-};
-
 export const updateWebId = webId => {
   return {
     type: "UPDATE_WEB_ID",
--- src/store/reducers/AuthReducer.js
+++ src/store/reducers/AuthReducer.js
@@ -4,13 +4,6 @@ const initState = {
 
 const authReducer = (state = initState, action) => {
   switch (action.type) {
-    case "LOG_IN":
-      return {
-        ...state,
-        userWebId: action.payload
-      };
-    case "LOG_OUT":
-      return { ...state };
     case "UPDATE_WEB_ID":
       return {
         ...state,
