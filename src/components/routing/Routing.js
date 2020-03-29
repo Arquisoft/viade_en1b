fd7ab4b42baca243d59cc43cfbb8be1b108a58ac
Marcos Alvarez <marcosalvarez@iMac-de-Marcos.local>
2020-03-29 19:06:45 +0200

    Not working share route

--- src/components/routing/Routing.js
+++ src/components/routing/Routing.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React from "react";
 import { HashRouter, Switch, Route, Redirect } from "react-router-dom";
 import Login from "../layout/login/Login";
 import MyProfile from "../user/myProfile/MyProfile";
@@ -10,21 +10,51 @@ import Settings from "../layout/settings/Settings";
 import ShareRoute from "../routes/shareRoute/ShareRoute";
 
 export default function Routing(props) {
-    const {navBar} = props;
-    return (
+  const { navBar } = props;
+  return (
     <HashRouter>
-        {navBar}
-        <Switch>
-            <Route exact path="/404" component={PageNotFound} />
-            <Route exact path="/" render={props => <Login {...props} />}></Route>
-            <Route exact path="/profile" render={() => <MyProfile {...props}/>}></Route>
-            <Route exact path="/dashboard" render={() => <Dashboard {...props} />}></Route>
-            <Route exact path="/routes/upload" render={() => <UploadRoute {...props}/>}></Route>
-            <Route exact path="/settings" render={() => <Settings {...props} changeTheme={()=>{}} changeLanguage={()=>{}}/>}></Route>
-            <Route exact path="/routes/MyRoutes" render={() => <MyRoutes {...props}/>}></Route>
-            <Route exact path="/routes/ShareRoute" render={() => <ShareRoute {...props}/>}></Route>
-            <Redirect to="/404" />
-          </Switch>
+      {navBar}
+      <Switch>
+        <Route exact path="/404" component={PageNotFound} />
+        <Route exact path="/" render={props => <Login {...props} />}></Route>
+        <Route
+          exact
+          path="/profile"
+          render={() => <MyProfile {...props} />}
+        ></Route>
+        <Route
+          exact
+          path="/dashboard"
+          render={() => <Dashboard {...props} />}
+        ></Route>
+        <Route
+          exact
+          path="/routes/upload"
+          render={() => <UploadRoute {...props} />}
+        ></Route>
+        <Route
+          exact
+          path="/settings"
+          render={() => (
+            <Settings
+              {...props}
+              changeTheme={() => {}}
+              changeLanguage={() => {}}
+            />
+          )}
+        ></Route>
+        <Route
+          exact
+          path="/routes/MyRoutes"
+          render={() => <MyRoutes {...props} />}
+        ></Route>
+        <Route
+          exact
+          path="/routes/shareRoute"
+          render={() => <ShareRoute {...props} />}
+        ></Route>
+        <Redirect to="/404" />
+      </Switch>
     </HashRouter>
-    );
-};
+  );
+}
