d1520ec6ff9f071df9a46f04963c05cf1215b0e2
César García Cabeza <cesareogarcia@hotmail.es>
2020-03-31 16:38:41 +0200

    Dashboard test

---
+++ src/components/dashboard/tests/Dashboard.test.js
@@ -0,0 +1,38 @@
+import {
+    render,
+    fireEvent,
+    queryByTestId,
+    waitForElement
+  } from "@testing-library/react";
+  import "@testing-library/jest-dom";
+import React from "react";
+import Dashboard from "../Dashboard";
+import { testStore } from "../../../utils";
+import rootReducer from "../../../store/reducers/RootReducer";
+import { Provider } from "react-redux";
+import { HashRouter } from "react-router-dom";
+
+describe("Dashboard Component", () => {
+
+    let wrapper;
+    const mockFn = jest.fn();
+    beforeEach(() => {
+        const initState = {
+            route: {},
+            auth: {},
+            user: {},
+            control: {}
+          };
+        const store = testStore(rootReducer, initState);
+        const {container} = render(<Provider store={store}>
+            <HashRouter>
+                <Dashboard routes ={[]} selectedRoute={""} showRoute={mockFn}></Dashboard>
+            </HashRouter>
+        </Provider>);
+        wrapper = container;
+    });
+
+    test("renders correctly", () => {
+        expect(queryByTestId(wrapper, "dashboard-container")).not.toBeNull();
+    });
+});
