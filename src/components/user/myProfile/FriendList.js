8e8708426ed69712ade3f179ea3a09131187c490
Marcos Alvarez <marcosalvarez@iMac-de-Marcos.local>
2020-04-01 19:57:37 +0200

    FriendList tests are passing

--- src/components/user/myProfile/FriendList.js
+++ src/components/user/myProfile/FriendList.js
@@ -2,17 +2,21 @@ import React from "react";
 import { Card, Form } from "react-bootstrap";
 import style from "./FriendList.css";
 import { BsCheck } from "react-icons/bs";
+import { Redirect } from "react-router-dom";
 
 export const FriendList = props => {
   //Function used by the non-checked version of the component
-  const openProfile = friendProfile => {
-    window.location.href = friendProfile + "profile/card#me";
+  const onClick = friend => {
+    return <Redirect to={friend.uri} />;
   };
-
   //Checked friendList to be used by the ShareRoute component
   let friendsCardsWithCheckBoxes = props.friends.map((friend, key) => {
     return (
-      <Card onClick={() => props.onClick(key)} key={key}>
+      <Card
+        data-testid={"card" + key}
+        onClick={() => props.onClick(key)}
+        key={key}
+      >
         <Card.Body style={{ marginLeft: "1em" }}>
           <Form.Check
             onChange={() => {}}
@@ -23,6 +27,7 @@ export const FriendList = props => {
                 <React.Fragment>
                   {friend.name}
                   <BsCheck
+                    data-testid={"friend-list-check" + key}
                     style={{
                       color: "yellowgreen",
                       transform: "scale(2)",
@@ -46,13 +51,16 @@ export const FriendList = props => {
   let friendsCards = props.friends.map((friend, index) => {
     return (
       <Card
-        data-testid="friend-list-card"
         key={index}
-        onClick={() => openProfile(`${friend.uri}`)}
+        onClick={friend => onClick(friend)}
+        data-testid={"friend-list-card" + index}
       >
         <Card.Body>
-          <Card.Link data-testid="friend-list-friend-uri" href={friend.uri}>
-            <p data-testid="friend-list-friend-name">{friend.name}</p>
+          <Card.Link
+            data-testid={"friend-list-friend-uri" + index}
+            href={friend.uri}
+          >
+            <p data-testid={"friend-list-friend-name" + index}>{friend.name}</p>
           </Card.Link>
         </Card.Body>
       </Card>
--- src/components/user/myProfile/tests/FriendList.test.js
+++ src/components/user/myProfile/tests/FriendList.test.js
@@ -1,49 +1,64 @@
-import {
-    render,
-    waitForElement,
-    queryByTestId,
-    fireEvent
-  } from "@testing-library/react";
-  import "@testing-library/jest-dom";
+import { render, fireEvent, screen, wait } from "@testing-library/react";
+import "@testing-library/jest-dom";
 import React from "react";
-import {FriendList} from "../FriendList";
+import { FriendList } from "../FriendList";
+import TestingRouter from "../../../routing/utils/TestingRouter";
 
+const mockOnClick = jest.fn();
+const mockFriends = [
+  { name: "mockFriend", uri: "https://viandetest2020.solid.community" },
+  {
+    name: "mockFriend2",
+    uri: "https://viandetest2020.solid.community",
+    checked: true
+  }
+];
+beforeEach(() => {
+  window.location = { href: mockFriends[0].uri + "profile/card#me" };
+});
 describe("FriendList component", () => {
+  test("renders correctly with normal setting", () => {
+    render(<FriendList friends={mockFriends}></FriendList>);
+    expect(screen.queryByTestId("friend-list-container")).toBeInTheDocument();
+    expect(screen.queryByTestId("friend-list-heading")).toBeInTheDocument();
+    expect(screen.queryByTestId("friend-list-main")).toBeInTheDocument();
+    expect(screen.queryByTestId("friend-list-card0")).toBeInTheDocument();
+    expect(screen.queryByTestId("friend-list-friend-uri0")).toBeInTheDocument();
 
-    const mockFriends = [
-        {name: "mockFriend", uri:"https://viandetest2020.solid.community"}
-    ];
+    expect(
+      screen.queryByTestId("friend-list-friend-name0")
+    ).toBeInTheDocument();
+    expect(screen.queryByTestId("friend-list-card1")).toBeInTheDocument();
+    expect(screen.queryByTestId("friend-list-friend-uri1")).toBeInTheDocument();
 
-    let wrapper;
+    expect(
+      screen.queryByTestId("friend-list-friend-name1")
+    ).toBeInTheDocument();
+  });
+  test("renders correctly with checkbox setting", () => {
+    render(
+      <FriendList
+        checked
+        onClick={mockOnClick}
+        friends={mockFriends}
+      ></FriendList>
+    );
+    expect(screen.queryByTestId("friend-list-main")).toBeInTheDocument();
+    expect(screen.queryByTestId("card0")).toBeInTheDocument();
 
-    const {location} = window;
-
-    beforeEach(() => {
-        delete window.location;
-        window.location = {href:mockFriends[0].uri+"profile/card#me"};
-        const {container} = render(<FriendList friends={mockFriends}></FriendList>);
-        wrapper = container;
-    });
-
-    afterAll(() => {
-        window.location = location;
-    });
-    
-    test("renders correctly", () => {
-        waitForElement(() => {
-            expect(queryByTestId(wrapper, "friend-list-container")).not.toBeNull();
-            expect(queryByTestId(wrapper, "friend-list-heading")).not.toBeNull();
-            expect(queryByTestId(wrapper, "friend-list-main")).not.toBeNull();
-            expect(queryByTestId(wrapper, "friend-list-card")).not.toBeNull();
-            expect(queryByTestId(wrapper, "friend-list-friend-uri")).not.toBeNull();
-            expect(queryByTestId(wrapper, "friend-list-firned-name")).not.toBeNull();
-        });
-    });
-
-    test("change location", () => {
-        waitForElement(() => {
-            fireEvent.click(queryByTestId(wrapper, "friend-list-card"));
-            expect(window.location.href).toBe(mockFriends[0].uri+"profile/card#me");
-        });
-    });
-}); 
+    expect(mockOnClick).not.toBeCalled();
+    fireEvent.click(screen.queryByTestId("card0"));
+    expect(mockOnClick).toBeCalled();
+    expect(screen.queryByTestId("friend-list-check1")).toBeInTheDocument();
+  });
+  test("change location", () => {
+    const { container } = render(
+      <TestingRouter
+        redirectUrl={mockFriends[0].uri}
+        ComponentWithRedirection={() => <FriendList friends={mockFriends} />}
+      />
+    );
+    const redirectUrl = mockFriends[0].uri;
+    expect(container.innerHTML).toEqual(expect.stringContaining(redirectUrl));
+  });
+});
