011d607e2465b61dcc79e8ee492571e0c4c08590
Álvaro <alvarogarciainfante@gmail.com>
2020-04-24 10:46:36 +0200

    Some changes in the cucumber testing, added an scenario

--- features/login.feature
+++ features/login.feature
@@ -1,10 +1,15 @@
-Feature: Logging in
+Feature: Viade_en1b
     Scenario: The user is not already logged in
         Given We visit the "http://localhost:3000/viade_en1b"
         And We put our page "https://viandetest2020.solid.community/profile/card#me", we put the credentials username "ViandeTest2020" and password "uO257611__"
-        Then The home page appears
-
-    Scenario: The user is not already logged in
-        Given We are logged in
         When We push the Upload Route button
         Then We can see the form to upload route
+
+    Scenario: The user is not already logged in
+        Given We log in
+        When We push the My routes button
+        Then We can see the routes
+
+       
+
+
---
+++ features/step-definition/upload.js
@@ -0,0 +1,109 @@
+const { Given, When, Then} = require("cucumber");
+const { Builder, By, Capabilities, Key, until } = require('selenium-webdriver');
+const { expect } = require('chai');
+const { firefox } = require("selenium-webdriver/firefox");
+var webdriver = require("selenium-webdriver");
+require("chromedriver");
+path = "C:\Users\alvar\geckodriver-v0.26.0-win64.exe";
+const capabilities = Capabilities.chrome();
+var chromeOptions = {
+  'args': ['--test-type', '--start-maximized']
+}
+capabilities.set('chromeOptions', chromeOptions);
+const driver = new webdriver.Builder().forBrowser('chrome').build();
+var originalWindow = null;
+
+
+//FIRST FEATURE
+Given('We visit the {string}', function (string) {
+  driver.get(string);
+  var button = driver.findElement(By.xpath("//*[contains(text(), 'Login here!')]"));
+  expect(button != null);
+});
+
+Given('We put our page {string}, we put the credentials username {string} and password {string}', function (string, string2, string3) {
+  driver.findElement(By.xpath("/html/body/div/div/div[1]/div[3]/button")).click();
+  driver.wait(function () {
+    return driver.getAllWindowHandles().then(function (handles) {
+      var isHandleCount2 = (handles.length == 2);
+      if (isHandleCount2) {
+        driver.switchTo().window(handles[1]);
+      }
+      return isHandleCount2;
+    });
+  }).then(function () {
+    var input = driver.findElement(By.xpath("/html/body/div/div/form/input"));
+    input.sendKeys(string);
+    driver.findElement(By.xpath("/html/body/div/div/form/button")).click();
+    driver.wait(until.elementsLocated(By.xpath("/html/body/div/div[2]/div[1]/div[1]/div/form/div[1]/div/input")), 10000).then(
+      function () {
+        driver.findElement(By.xpath("/html/body/div/div[2]/div[1]/div[1]/div/form/div[1]/div/input")).sendKeys(string2);
+        driver.findElement(By.xpath("/html/body/div/div[2]/div[1]/div[1]/div/form/div[2]/div/input")).sendKeys(string3);
+        var loginButton = driver.findElement(By.xpath("/html/body/div/div[2]/div[1]/div[1]/div/form/button"));
+        loginButton.click();
+      })
+  })
+});
+
+
+When('We push the Upload Route button', function () {
+  driver.wait(function () {
+    return driver.getAllWindowHandles().then(function (handles) {
+      var isHandleCount2 = (handles.length == 2);
+      originalWindow = handles[0];
+      if (isHandleCount2) {
+        driver.switchTo().window(handles[1]);
+      }
+      return isHandleCount2;
+    });
+  }).then(function () {
+    driver.wait(function () {
+      return driver.getAllWindowHandles().then(function (handles) {
+        var isHandleCount1 = (handles.length == 1);
+        originalWindow = handles[0];
+        if (isHandleCount1) {
+          driver.switchTo().window(handles[0]);
+          //here we do the things
+          driver.wait(until.elementsLocated(By.xpath("/html/body/div/div/nav/div/div/span[3]/a")), 100000).then(
+            function () {
+              var buttonUpload = driver.findElement(By.xpath("/html/body/div/div/nav/div/div/span[3]/a"));
+              buttonUpload.click();
+            }
+          )
+        }
+        return isHandleCount1;
+      });
+    })
+  });
+});
+
+Then('We can see the form to upload route', function () {
+  driver.wait(function () {
+    return driver.getAllWindowHandles().then(function (handles) {
+      var isHandleCount2 = (handles.length == 2);
+      originalWindow = handles[0];
+      if (isHandleCount2) {
+        driver.switchTo().window(handles[1]);
+      }
+      return isHandleCount2;
+    });
+  }).then(function () {
+    driver.wait(function () {
+      return driver.getAllWindowHandles().then(function (handles) {
+        var isHandleCount1 = (handles.length == 1);
+        originalWindow = handles[0];
+        if (isHandleCount1) {
+          driver.switchTo().window(handles[0]);
+          //here we do the things
+          driver.wait(until.elementsLocated(By.xpath("/html/body/div/div/div[1]/form/div[1]/div[3]/textarea")), 100000).then(
+            function () {
+              var buttonChooseARoute = driver.findElement(By.xpath("/html/body/div/div/div[1]/form/div[1]/div[3]/textarea"));
+              expect(buttonChooseARoute!=null);
+            }
+          )
+        }
+        return isHandleCount1;
+      });
+    })
+  });
+});
