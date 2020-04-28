af7c1a705eddde45b280b07b5afa23260ee71014
César García Cabeza <UO257314@uniovi.es>
2020-04-28 10:57:47 +0200

    UnShare test redux

--- src/store/reducers/tests/RouteReducer.test.js
+++ src/store/reducers/tests/RouteReducer.test.js
@@ -88,11 +88,24 @@ describe("Routes Reducer", () => {
                 { id: 4, name: "Senda del Oso", author: "César", positions: [[43.360383711, -5.850650009],[43.35763791, -5.842024025],[43.360976539, -5.831938919],[43.366405318, -5.837775406],[43.361382154, -5.844255623]], description: "", images: [], videos: [], sharedWith: [] },
             ];
         
-            const expected = {
-                routes : routes,
-                selectedRoute: null
-            };
-            expect(newState).toEqual(expected);
+            expect(newState).toEqual(initState);
+        });
+
+        test("Type UNSHARE_ROUTE", () => {
+            let mockRoute = { id: 0, name: "Hiking Naranco ", author: "César", positions: [[43.360383711, -5.850650009],[43.35763791, -5.842024025],[43.360976539, -5.831938919],[43.366405318, -5.837775406],[43.361382154, -5.844255623]], description: "A beautiful landscape for a beautiful country like Spain. Vegetation is incredible, wildlife is amazing", images: ["https://source.unsplash.com/random/600x600", "https://source.unsplash.com/random/602x602"], videos: ["futuro video 1", "futuro video 2"], sharedWith: [] };
+            const mockAuthorWebId = "https://elbicho.solid.community/profile/card#me";
+            const newState = routeReducer(initState, {
+                type : "UNSHARE_ROUTE",
+                payload: { routeId: mockRoute.id, authorWebId: mockAuthorWebId, userWebId: mockAuthorWebId}
+            });
+            const routes = [
+                { id: 1, name: "Hiking Ruta de las Xanas", author: "Marcos", positions: [[43.360383711, -5.850],[43.35763791, -5.842024025],[43.360976539, -5.831938919],[43.366405318, -5.837775406],[43.361382154, -5.844255623]], description: "", images: [], videos: [], sharedWith: [] },
+                { id: 2, name: "Senda del Oso", author: "César", positions: [[43.360383711, -5.850650009],[43.35763791, -5.842024025],[43.360976539, -5.831938919],[43.366405318, -5.837775406],[43.361382154, -5.844255623]], description: "", images: [], videos: [], sharedWith: [] },
+                { id: 3, name: "Hiking Naranco", author: "César", positions: [[43.360383711, -5.850650009],[43.35763791, -5.842024025],[43.360976539, -5.831938919],[43.366405318, -5.837775406],[43.361382154, -5.844255623]], description: "", images: [], videos: [], sharedWith: [] },
+                { id: 4, name: "Senda del Oso", author: "César", positions: [[43.360383711, -5.850650009],[43.35763791, -5.842024025],[43.360976539, -5.831938919],[43.366405318, -5.837775406],[43.361382154, -5.844255623]], description: "", images: [], videos: [], sharedWith: [] },
+            ];
+            expect(newState.routes).toEqual(routes);
+
         });
 
         const mockPayload = "";
