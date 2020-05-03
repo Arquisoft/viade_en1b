ceadf2ab7258d5e68f949e4ea0b5b902dd285dfa
InigoGutierrez <inigogf.95@gmail.com>
2020-05-03 17:43:17 +0200

    Solved a couple quality issues.

--- src/store/reducers/tests/RouteReducer.test.js
+++ src/store/reducers/tests/RouteReducer.test.js
@@ -4,9 +4,9 @@ import { uploadRoute } from "../../actions/RouteActions";
 
 describe("Routes Reducer", () => {
 
-    const route0 = { id: 0, name: "Hiking Naranco", author: "César", positions: [[43.360383711, -5.850650009],[43.35763791, -5.842024025],[43.360976539, -5.831938919],[43.366405318, -5.837775406],[43.361382154, -5.844255623]], description: "A beautiful landscape for a beautiful country like Spain. Vegetation is incredible, wildlife is amazing", images: ["https://source.unsplash.com/random/600x600", "https://source.unsplash.com/random/602x602"], videos: ["futuro video 1", "futuro video 2"], sharedWith: [] };
-    const route1 = { id: 1, name: "Hiking Ruta de las Xanas", author: "Marcos", positions: [[43.360383711, -5.850],[43.35763791, -5.842024025],[43.360976539, -5.831938919],[43.366405318, -5.837775406],[43.361382154, -5.844255623]], description: "", images: [], videos: [], sharedWith: [] };
-    const route2 = { id: 2, name: "Senda del Oso", author: "César", positions: [[43.360383711, -5.850650009],[43.35763791, -5.842024025],[43.360976539, -5.831938919],[43.366405318, -5.837775406],[43.361382154, -5.844255623]], description: "", images: [], videos: [], sharedWith: [] };
+    const route0 = { id: 0, name: "Hiking Naranco", author: "César", positions: [[43.360383711, -5.840650009],[43.35763791, -5.842024025],[43.360976539, -5.831938919],[43.366405318, -5.837775406],[43.361382154, -5.844255623]], description: "A beautiful landscape for a beautiful country like Spain. Vegetation is incredible, wildlife is amazing", images: ["https://source.unsplash.com/random/600x600", "https://source.unsplash.com/random/602x602"], videos: ["futuro video 1", "futuro video 2"], sharedWith: [] };
+    const route1 = { id: 1, name: "Hiking Ruta de las Xanas", author: "Marcos", positions: [[43.360383711, -5.840],[43.35763791, -5.842024025],[43.360976539, -5.831938919],[43.366405318, -5.837775406],[43.361382154, -5.844255623]], description: "", images: [], videos: [], sharedWith: [] };
+    const route2 = { id: 2, name: "Senda del Oso", author: "César", positions: [[43.360383711, -5.840650009],[43.35763791, -5.842024025],[43.360976539, -5.831938919],[43.366405318, -5.837775406],[43.361382154, -5.844255623]], description: "", images: [], videos: [], sharedWith: [] };
     let route3 = JSON.parse(JSON.stringify(route0));
     route3.id = 3;
     route3.description = "";
@@ -23,7 +23,7 @@ describe("Routes Reducer", () => {
     ];
 
     const initState = {
-        routes : routes,
+        routes,
         selectedRoute: null
     };
 
@@ -141,7 +141,7 @@ describe("Routes Reducer", () => {
             const newState = routeReducer(initNewState, {
                 type: "LOAD_ROUTES_SUCCESS",
                 payload: mockPayload
-            })
+            });
 
             const expected = {
                 routes: mockPayload,
