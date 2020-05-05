e3d0b0a7d6d340041bf23dcfc1984e4e39fa17c3
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-05-05 08:24:02 +0000

    Deploy Arquisoft/viade_en1b to github.com/Arquisoft/viade_en1b.git:gh-pages

---
+++ precache-manifest.545c4082797b832a407ccad70c13f20b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de47ad5efbc8bbd5cbcff78f7f745f67",
+    "url": "/viade_en1b/index.html"
+  },
+  {
+    "revision": "d1629a288efa450fe118",
+    "url": "/viade_en1b/static/css/2.856d6754.chunk.css"
+  },
+  {
+    "revision": "f76ad8bbfec46849b278",
+    "url": "/viade_en1b/static/css/main.940d6c3c.chunk.css"
+  },
+  {
+    "revision": "d1629a288efa450fe118",
+    "url": "/viade_en1b/static/js/2.4ab9edbd.chunk.js"
+  },
+  {
+    "revision": "0835cebcd7a995884fa9d4ba118a77bd",
+    "url": "/viade_en1b/static/js/2.4ab9edbd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f76ad8bbfec46849b278",
+    "url": "/viade_en1b/static/js/main.43e0fe03.chunk.js"
+  },
+  {
+    "revision": "8c73fe3596a7759d649d",
+    "url": "/viade_en1b/static/js/runtime-main.88d57bd4.js"
+  }
+]);
